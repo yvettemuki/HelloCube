c39c40d6622eb24dcaccdccd04431334e50cad73
yvettemuki <373628977@qq.com>
2021-02-05 00:53:46 +0800

    add: add texture

--- src/1.0.cube.fs.glsl
+++ src/1.0.cube.fs.glsl
@@ -1,8 +1,13 @@
 #version 330 core
 in vec3 myColor;
+in vec2 myTexCoord;
+
 out vec4 FragColor;
+
+uniform sampler2D texture1;
 
 void main()
 {
-    FragColor = vec4(myColor, 1.0f);
+//    FragColor = vec4(myColor, 1.0f);
+    FragColor = texture(texture1, myTexCoord);
 }
--- src/1.0.cube.vs.glsl
+++ src/1.0.cube.vs.glsl
@@ -2,7 +2,10 @@
 
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aTexCoord;
+
 out vec3 myColor;
+out vec2 myTexCoord;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -21,4 +24,5 @@ void main()
     } else {
         myColor = aColor;
     }
+    myTexCoord = aTexCoord;
 }
